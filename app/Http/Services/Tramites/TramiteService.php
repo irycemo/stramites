1b5568d8f4942c8af994a90712ee163a6b62c60e
kikorobledo <enrique_j_@hotmail.com>
2026-01-23 09:51:35 -0600

    Fix recepcion

--- app/Http/Services/Tramites/TramiteService.php
+++ app/Http/Services/Tramites/TramiteService.php
@@ -192,7 +192,7 @@ class TramiteService{
         $this->tramite->update([
             'estado' => 'pagado',
             'fecha_pago' => $this->convertirFecha($fecha),
-            'fecha_prelacion' => now()->format('Y-m-d H:i:s'),
+            'fecha_prelacion' => now()->toDateString(),
             'documento_de_pago' => $documento,
             'fecha_entrega' => $this->calcularFechaEntrega()
         ]);
