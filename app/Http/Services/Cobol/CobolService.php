b0adf89d9df42e6e15acdbbcc1ca88ec3cb568b7
kikorobledo <enrique_j_@hotmail.com>
2026-07-31 12:50:49 -0600

    fix cobolservice

--- app/Http/Services/Cobol/CobolService.php
+++ app/Http/Services/Cobol/CobolService.php
@@ -12,9 +12,9 @@ class CobolService{
 
         $usuario = str_pad($tramite->creadoPor->name , 30, ' ');
 
-        $string = $tramite->orden_de_pago . $tramite->fecha_pago->format('Ymd') . $usuario . ($tramite->monto * 100) . $tramite->fecha_entrega->format('Ymd');
+        $string = $tramite->documento_de_pago . $tramite->fecha_pago->format('Ymd') . $usuario . ($tramite->monto * 100) . $tramite->fecha_entrega->format('Ymd');
 
-        $nombre = $tramite->procedencia . $tramite->orden_de_pago . '.TXT';
+        $nombre = $tramite->procedencia . $tramite->documento_de_pago . '.TXT';
 
         Storage::disk('s3')->put('cobol/salida/'. $nombre, $string);
 
