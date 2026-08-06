32b82bb5dc275e77bf294c6741919edc40bbaf54
kikorobledo <enrique_j_@hotmail.com>
2026-08-06 14:22:39 -0600

    Fix cobol service

--- app/Http/Services/Cobol/CobolService.php
+++ app/Http/Services/Cobol/CobolService.php
@@ -10,11 +10,27 @@ class CobolService{
 
     public function insertarS3(Tramite $tramite){
 
+        $recibo_pago = (string) $tramite->documento_de_pago;
+
+        if(strlen($recibo_pago) < 10){
+
+            $recibo_pago = str_pad($recibo_pago, 10, '0', STR_PAD_LEFT);
+
+            $recibo_pago[0] = "1";
+
+        }
+
+        $fecha_pago = $tramite->fecha_pago->format('Ymd');
+
         $usuario = str_pad($tramite->creadoPor->name , 30, ' ');
 
-        $string = $tramite->documento_de_pago . $tramite->fecha_pago->format('Ymd') . $usuario . ($tramite->monto * 100) . $tramite->fecha_entrega->format('Ymd');
+        $monto = str_pad($tramite->monto * 100, 14, '0', STR_PAD_LEFT);
 
-        $nombre = $tramite->procedencia . $tramite->documento_de_pago . '.TXT';
+        $fecha_entrega = $tramite->fecha_entrega->format('Ymd');
+
+        $string = $recibo_pago . $fecha_pago . $usuario . $monto . $fecha_entrega;
+
+        $nombre = $tramite->procedencia . $recibo_pago;
 
         Storage::disk('s3')->put('cobol/salida/'. $nombre, $string);
 
