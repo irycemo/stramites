8eaecc36bd711aca5896c0e0398f7ad51d7c6f24
kikorobledo <enrique_j_@hotmail.com>
2026-09-23 16:10:33 -0600

    Se agrego función para revisar lineas de captura pagadas

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TramitesController;
+use Illuminate\Console\Scheduling\Schedule;
 use App\Http\Services\LineasDeCaptura\LineaCapturaApi;
 use App\Models\Tramite;
 use Illuminate\Foundation\Inspiring;
