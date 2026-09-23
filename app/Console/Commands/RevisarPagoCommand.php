a0776d046190e320826d00e0ab9b42201a857d44
kikorobledo <enrique_j_@hotmail.com>
2026-09-23 16:19:21 -0600

    Se agrego función para revisar lineas de captura pagadas

--- app/Console/Commands/RevisarPagoCommand.php
+++ app/Console/Commands/RevisarPagoCommand.php
@@ -28,7 +28,7 @@ class RevisarPagoCommand extends Command
     public function handle()
     {
 
-        $tramites = Tramite::with('servicio.categoria', 'adicionaAlTramite')
+        $tramites = Tramite::with('servicio.categoria', 'adicionaAlTramite', 'creadoPor:id,name')
                                 ->where('estado', 'nuevo')
                                 ->get();
 
